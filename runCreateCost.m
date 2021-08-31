0cec34c99acc292b1823c63f17ee770b301ea919
Yonit <yonit.m@campus.technion.ac.il>
2021-08-31 11:00:23 +0300

    Updated runCreateCost.m to hold options for both one and two layers in the same script. Added function runLayerSeparationFull.m to run full pipeline of layer separation (cost,surface detection, and surface projection).

--- runCreateCost.m
+++ runCreateCost.m
@@ -13,16 +13,13 @@ use_CLAHE = 1;  % Default: 1, set to 0 if don't want to use CLAHE to normalise g
 norm_window = 4; % Default: 4. norm_window*blocksigma is the length scale for normalisation of gradient using CLAHE. 
                  % Decreasing can sometimes help prevent jumps between surfaces.
 saveDiffused = 0; % Set to one to save diffused images, and to zero to not save.                 
-
+numLayers = 2 ; % Set to 2 for two layers (default), and set to 1 for single layer.
 %% Define directories of original images to run over folders and create cost images (original images should be 3D image stacks saved as separate timepoints). 
-
 
 topMainDir='\\phhydra\TempData\SD\Yonit\2020\2020_10\2020_10_29\TIFF Files\'; % main folder of original files for layer separation
 mainDirList= { ... % enter in the following line all the  movie dirs for cost calculation.
 
-
 'Pos_15\C0\',...
-
 
 };
 for i=1:length(mainDirList),mainInDirList{i}=[topMainDir,mainDirList{i}];end
@@ -30,10 +27,7 @@ for i=1:length(mainDirList),mainInDirList{i}=[topMainDir,mainDirList{i}];end
 topAnalysisDir='\\PHHYDRA\phhydraB\Analysis\users\Yonit\Movie_Analysis\Labeled_cells\'; % main folder for layer separation results
 mainAnalysisDirList= { ... % enter in the following line all the output dirs for cost calculation.
 
-% 
  '2020_10_29_pos15\', ...
-
-
 
 };
 for i=1:length(mainAnalysisDirList),AnalysisDirList{i}=[topAnalysisDir,mainAnalysisDirList{i}];end
@@ -48,6 +42,10 @@ for i=1:length(mainDirList)
     dirGradient=[analysisDir,'\Gradient'];
     dirDiffused=[analysisDir,'\Diffused'];
      
+    if numLayers == 1
+        dirGradient=[analysisDir,'\Gradient_1Layer'];
+    end
+        
     % make all the needed directories
     mkdir(analysisDir);
     cd(analysisDir); mkdir(dirGradient);
@@ -55,7 +53,7 @@ for i=1:length(mainDirList)
         mkdir(dirDiffused);
     end
     
-    save('CostParameters', 'z_scale','xy_scale','use_CLAHE','norm_window') % saves the variables including the analysis parameters used
+    save('CostParameters', 'z_scale','xy_scale','use_CLAHE','norm_window','numLayers') % saves the variables including the analysis parameters used
 
     cd (maskDir);
     tpoints = dir('*tif*');
@@ -64,11 +62,14 @@ for i=1:length(mainDirList)
      parfor j = 1:length(tpoints)
         name_end = find(tpoints(j).name == '.');
         thisFileImName = [tpoints(j).name(1:(name_end-1))]
+        if numLayers == 2
         CreateCost_with_CLAHE(thisFileImName, inputDir, analysisDir,maskDir, z_scale,xy_scale,outputZScale, use_CLAHE, norm_window, saveDiffused);
+        elseif numLayers == 1
+        CreateCost_Single_Layer(thisFileImName, inputDir, analysisDir,maskDir, z_scale,xy_scale,outputZScale, use_CLAHE, norm_window, saveDiffused);
+        end
         
-    end
-    
-      
+     end
+  
 end
 
 
